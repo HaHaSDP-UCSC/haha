954aab6d97bfe36fb20390b1673de31a70fccac8
Kevin Lee <keclee@ucsc.edu>
2017-04-14 03:12:38 -0700

    Import some stuff from Linux.

--- HaHa/friend/friendlist.h
+++ HaHa/friend/friendlist.h
@@ -1,6 +1,6 @@
 /*
  * @file friendlist.h
- * @brief List of Neighbors with accompanying data.
+ * @brief List of friends with accompanying data.
  * @author Kevin Lee
  * @date 4/8/2017 4:37:47 PM
  */ 
@@ -8,11 +8,16 @@
 #ifndef FRIENDLIST_H_
 #define FRIENDLIST_H_
 
+#define MAXFIRSTNAME 17
+#define MAXLASTNAME 17
+#define MAXNETADDR 0 //TODO fix.
+#define MAXPORT 2
+
 typedef struct {
 	uint8_t id; //Internal ID.
-	char firstname[FIRSTNAME];
-	char lastname[LASTNAME];
-	char networkaddr[NETADDR]; //TODO Finalize data structure.
+	char firstname[MAXFIRSTNAME];
+	char lastname[MAXLASTNAME];
+	char networkaddr[MAXNETADDR]; //TODO Finalize data structure.
 	uint16_t port; //Network port.
 	long lastresponse; //TODO time data structure
 	uint16_t responseflag; //16 Available flags for expected responses.
---
+++ HaHa/messagequeue/packet.h
@@ -0,0 +1,62 @@
+/*
+ * @file packet.h
+ * @brief Packet creation and handling.
+ * @author Kevin Lee
+ * @date 4/14/2017
+ */ 
+
+#ifndef PACKET_H_
+#define PACKET_H_
+
+typedef enum {
+	PING_REQUEST = 0x01,
+	HELP_REQUEST,
+	HELP_RESPONSE,
+	HELP_FROM_ANYONE_REQUEST,
+	HELP_FROM_ANYONE_RESPONSE,
+	FIND_HOPS_REQUEST,
+	FIND_HOPS_RESPONSE,
+	FIND_NEIGHBORS_REQUEST,
+	FIND_NEIGHBORS_RESPONSE,
+	FRIEND_REQUEST,
+	FRIEND_RESPONSE,
+	UNFRIEND_REQUEST
+} op;
+
+typedef unsigned char opcode;
+typedef unsigned char ttl;
+typedef uint16_t uid;
+
+typedef struct {
+	//Packet params here
+	//opcode opcode;
+	opcode opcode;
+	flags flags;
+	uid SRCUID;
+	uid DESTUID;
+	uid ORIGINUID;
+	char SRCFIRSTNAME[MAXFIRSTNAME]; //TODO optimize for space.
+	char SRCLASTNAME[MAXFIRSTNAME]; //TODO optimize for space.
+	char SRCPHONE[MAXPHONE]; //TODO optimize for space.
+	char SRCHOMEADDR[MAXHOMEADDR]; //TODO optimize for space.
+	ttl ttl;
+} Packet;
+
+int pingHandler(Packet p);
+
+int helpRequestHandler(Packet p);
+int helpResponseHandler(Packet p);
+int helpFromAnyoneRequestHandler(Packet p);
+int helpFromAnyoneResponseHandler(Packet p);
+
+int findHopsRequestHandler(Packet p);
+int findHopsResponseHandler(Packet p);
+
+int findNeighborsRequestHandler(Packet p);
+int findNeighborsResponseHandler(Packet p);
+
+int friendRequestHandler(Packet p);
+int friendResponseHandler(Packet p);
+int unfriendRequestHandler(Packet p);
+
+#endif /* PACKET_H_ */
