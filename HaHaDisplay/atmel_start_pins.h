16d5760621ebeb22b0babc8aea2cca8376d01694
August Valera <4U6U57@gmail.com>
2017-04-14 18:51:35 -0700

    Initial commit for standalone repo

---
+++ HaHaDisplay/atmel_start_pins.h
@@ -0,0 +1,22 @@
+/*
+ * Code generated from Atmel Start.
+ *
+ * This file will be overwritten when reconfiguring your Atmel Start project.
+ * Please copy examples or other code you want to keep to a separate file
+ * to avoid losing it when reconfiguring.
+ */
+#ifndef ATMEL_START_PINS_H_INCLUDED
+#define ATMEL_START_PINS_H_INCLUDED
+
+#include <hal_gpio.h>
+
+#define DISP_BRIGHT GPIO(GPIO_PORTA, 13)
+#define DISP_B3 GPIO(GPIO_PORTA, 14)
+#define DISP_B2 GPIO(GPIO_PORTA, 15)
+#define DISP_B1 GPIO(GPIO_PORTA, 16)
+#define DISP_B0 GPIO(GPIO_PORTA, 17)
+#define DISP_E GPIO(GPIO_PORTA, 18)
+#define DISP_REG GPIO(GPIO_PORTA, 19)
+#define DISP_CTRST GPIO(GPIO_PORTA, 20)
+
+#endif // ATMEL_START_PINS_H_INCLUDED
---
+++ HaHaDisplay/driver_init.c
@@ -0,0 +1,144 @@
+/*
+ * Code generated from Atmel Start.
+ *
+ * This file will be overwritten when reconfiguring your Atmel Start project.
+ * Please copy examples or other code you want to keep to a separate file
+ * to avoid losing it when reconfiguring.
+ */
+
+#include "driver_init.h"
+#include <utils.h>
+#include <hal_init.h>
+#include <hpl_irq.h>
+
+#if CONF_DMAC_MAX_USED_DESC > 0
+#include <hpl_dma.h>
+#include <hpl_prov_dma_ctrl_v100.h>
+
+COMPILER_ALIGNED(16)
+DmacDescriptor _descriptor_section[CONF_DMAC_MAX_USED_DESC] SECTION_DMAC_DESCRIPTOR;
+
+struct _dma_resource _resource[CONF_DMAC_MAX_USED_CH];
+
+uint32_t dmac_ch_used = CONF_DMAC_MAX_USED_CH;
+#endif
+
+extern struct _irq_descriptor *_irq_table[PERIPH_COUNT_IRQn];
+extern void                    Default_Handler(void);
+
+void system_init(void)
+{
+	init_mcu();
+
+	// GPIO on LP_GPIO_13
+
+	// Set pin direction to output
+	gpio_set_pin_direction(DISP_BRIGHT, GPIO_DIRECTION_OUT);
+
+	gpio_set_pin_level(DISP_BRIGHT,
+	                   // <y> Initial level
+	                   // <id> pad_initial_level
+	                   // <false"> Low
+	                   // <true"> High
+	                   true);
+
+	gpio_set_pin_function(DISP_BRIGHT, GPIO_PIN_FUNCTION_OFF);
+
+	// GPIO on LP_GPIO_14
+
+	// Set pin direction to output
+	gpio_set_pin_direction(DISP_B3, GPIO_DIRECTION_OUT);
+
+	gpio_set_pin_level(DISP_B3,
+	                   // <y> Initial level
+	                   // <id> pad_initial_level
+	                   // <false"> Low
+	                   // <true"> High
+	                   true);
+
+	gpio_set_pin_function(DISP_B3, GPIO_PIN_FUNCTION_OFF);
+
+	// GPIO on LP_GPIO_15
+
+	// Set pin direction to output
+	gpio_set_pin_direction(DISP_B2, GPIO_DIRECTION_OUT);
+
+	gpio_set_pin_level(DISP_B2,
+	                   // <y> Initial level
+	                   // <id> pad_initial_level
+	                   // <false"> Low
+	                   // <true"> High
+	                   true);
+
+	gpio_set_pin_function(DISP_B2, GPIO_PIN_FUNCTION_OFF);
+
+	// GPIO on LP_GPIO_16
+
+	// Set pin direction to output
+	gpio_set_pin_direction(DISP_B1, GPIO_DIRECTION_OUT);
+
+	gpio_set_pin_level(DISP_B1,
+	                   // <y> Initial level
+	                   // <id> pad_initial_level
+	                   // <false"> Low
+	                   // <true"> High
+	                   true);
+
+	gpio_set_pin_function(DISP_B1, GPIO_PIN_FUNCTION_OFF);
+
+	// GPIO on LP_GPIO_17
+
+	// Set pin direction to output
+	gpio_set_pin_direction(DISP_B0, GPIO_DIRECTION_OUT);
+
+	gpio_set_pin_level(DISP_B0,
+	                   // <y> Initial level
+	                   // <id> pad_initial_level
+	                   // <false"> Low
+	                   // <true"> High
+	                   true);
+
+	gpio_set_pin_function(DISP_B0, GPIO_PIN_FUNCTION_OFF);
+
+	// GPIO on LP_GPIO_18
+
+	// Set pin direction to output
+	gpio_set_pin_direction(DISP_E, GPIO_DIRECTION_OUT);
+
+	gpio_set_pin_level(DISP_E,
+	                   // <y> Initial level
+	                   // <id> pad_initial_level
+	                   // <false"> Low
+	                   // <true"> High
+	                   true);
+
+	gpio_set_pin_function(DISP_E, GPIO_PIN_FUNCTION_OFF);
+
+	// GPIO on LP_GPIO_19
+
+	// Set pin direction to output
+	gpio_set_pin_direction(DISP_REG, GPIO_DIRECTION_OUT);
+
+	gpio_set_pin_level(DISP_REG,
+	                   // <y> Initial level
+	                   // <id> pad_initial_level
+	                   // <false"> Low
+	                   // <true"> High
+	                   true);
+
+	gpio_set_pin_function(DISP_REG, GPIO_PIN_FUNCTION_OFF);
+
+	// GPIO on LP_GPIO_20
+
+	// Set pin direction to output
+	gpio_set_pin_direction(DISP_CTRST, GPIO_DIRECTION_OUT);
+
+	gpio_set_pin_level(DISP_CTRST,
+	                   // <y> Initial level
+	                   // <id> pad_initial_level
+	                   // <false"> Low
+	                   // <true"> High
+	                   true);
+
+	gpio_set_pin_function(DISP_CTRST, GPIO_PIN_FUNCTION_OFF);
+}
