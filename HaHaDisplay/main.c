bf3c5106e124f0242215ac098309c626f071b7d6
August Valera <4U6U57@gmail.com>
2017-04-16 11:36:56 -0700

    Add hd44780 library

--- HaHaDisplay/main.c
+++ HaHaDisplay/main.c
@@ -6,6 +6,10 @@ int main(void)
 	atmel_start_init();
 
 	/* Replace with your application code */
+	lcd_init();
+	lcd_clrscr();
+	lcd_puts("Hello World...");
+	
 	while (1) {
 	}
 }
---
+++ HaHaDisplay/menu/lcd.c
@@ -0,0 +1,7 @@
+/*
+ * lcd.c
+ *
+ * Created: 2017-04-15 22:17:50
+ *  Author: August Valera
+ */ 
+
